7a170dfb345d2d3e872365eaf117fd764b404459
kobalt <>
2019-08-28 18:58:53 +0900

    added QuiverTrial on casks

---
+++ Casks/quiver.rb
@@ -0,0 +1,10 @@
+cask 'quiver' do
+  version :latest
+  sha256 :no_check
+
+  url "https://happenapps.com/downloads/QuiverFreeTrial.zip"
+  name 'QuiverTrial'
+  homepage 'https://happenapps.com'
+
+  app 'Quiver.app'
+end
