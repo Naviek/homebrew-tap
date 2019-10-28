3a86e9eb87ebbeabeb6bb6c3f108e8bc54884e10
kobalt <>
2019-10-28 13:45:54 +0900

    added totalspaces

---
+++ Casks/totalspaces.rb
@@ -0,0 +1,24 @@
+cask 'totalspaces' do
+  version '2.8.5'
+  sha256 '3cc987b3db42e931802d50c3ad6488219e9f3eb4832341d82c35f951af06e90c'
+
+  url "http://downloads.binaryage.com/TotalSpaces#{version.major}-#{version}.dmg"
+  appcast "http://updates-s3.binaryage.com/totalspaces#{version.major}.xml"
+  name 'TotalSpaces'
+  homepage 'https://totalspaces.binaryage.com/'
+
+  installer manual: "TotalSpaces#{version.major}.app"
+
+  uninstall pkgutil: "com.binaryage.TotalSpaces#{version.major}",
+            script:  {
+                       executable: "TotalSpaces#{version.major} Uninstaller.app/Contents/MacOS/TotalSpaces#{version.major} Uninstaller",
+                       args:       ['--headless'],
+                       sudo:       true,
+                     },
+            signal:  [
+                       ['INT', 'com.binaryage.totalspacescrashwatcher'],
+                       ['KILL', 'com.binaryage.totalspacescrashwatcher'],
+                     ],
+            quit:    "com.binaryage.TotalSpaces#{version.major}"
+end
+
